e115f270918e6c2e07bd209d1d81028762a5befc
nthskyradiated <andy.pandaan@outlook.com>
2024-05-18 14:32:26 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# LSD KIT
+
+### A Lucia / Sveltekit / Drizzle ORM template
+
+<pre>
+</pre>
+
+- OAuth(Github, Google), and Email/Password authentication methods already configured
+  \
+  &nbsp;
+
+- Password reset through email enabled (via Resend).
+  \
+  &nbsp;
+
+- Email verification also enabled (via resend).
+  \
+  &nbsp;
+
+- .env sample provided.
+  \
+  &nbsp;
+  
+  - Uses Zod for schema validation.   
---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import Button from '@/lib/components/ui/button/button.svelte';
+	import { goto } from '$app/navigation';
+</script>
+
+<h1>Welcome to LSD kit template</h1>
+<p>Visit LSD's<a href="https://github.com/nthskyradiated/lsd-kit">Github repo</a> to learn more.</p>
+
+<Button
+	type="button"
+	on:click={() => {
+		goto('auth/login');
+	}}>Log in with Email</Button
+>
+<Button
+	type="button"
+	on:click={() => {
+		goto('auth/register');
+	}}>Register</Button
+>
+<Button
+	type="button"
+	on:click={() => {
+		goto('/auth/github');
+	}}>Continue with Github</Button
+>
+<Button
+	type="button"
+	on:click={() => {
+		goto('/auth/google');
+	}}>Continue with Google</Button
+>
