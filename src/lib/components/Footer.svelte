3ff60bec49fb790cd663490cee4e89f8996d7298
nthskyradiated <andy.pandaan@outlook.com>
2024-06-21 00:01:21 +0400

    updated deps updated navbar

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,6 +1,6 @@
 <script>
 </script>
 
-<footer class="mx-auto mt-24 w-full text-center align-middle">
+<footer class="mx-auto mt-24 w-full text-center align-middle sticky top-full">
 	<span class="w-full pb-8 text-center">&copy; LSD-kit 2024 All Rights Reserved</span>
 </footer>
--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <header class="my-12 mb-32 ml-4 mr-10 flex w-full flex-row gap-4 text-start sm:ml-8">
-	<ThemeToggler />
+	<ThemeToggler side="bottom"/>
 	<div>
 		<h1 class="mb-2 text-3xl font-bold leading-none">LSD kit</h1>
 		<small class="my-4 text-sm text-gray-500">
